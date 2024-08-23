250c4d1659235055ab6d5731b3f3fa0cc961dc78
fauzanhaki <fauzanbaehaqi513@gmail.com>
2024-08-23 10:28:20 +0700

    add optimization

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
 import "./styles/main.css";
 import "./styles/responsive.css";
+
+const menu = document.querySelector("#menu");
+const drawer = document.querySelector("#drawer");
+
+menu.addEventListener("click", function (e) {
+  drawer.classList.toggle("open");
+  e.stopPropagation();
+});
+
+document.addEventListener("click", function () {
+  drawer.classList.remove("open");
+});
